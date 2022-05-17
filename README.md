f91863fc5b99b35ceaf30a12305479b326136bc9
jorgec <jorgec@meta4.com>
2022-05-17 16:19:15 +0200

    Stream I/O and Local Variables

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cpp_pluralsight 
+
+Learn to Program with C++ 17
+by Kate Gregory
+
+If you've never programmed before, and you think you'd like to learn C++, why not learn it first? This course covers what you need to start writing real applications in C++.
--- Small/Small.cpp
+++ Small/Small.cpp
@@ -1,20 +1,45 @@
-// Small.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using std::cout;
+using std::cin;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello Output!" << "\n";
+    std::cout << 34 << "\n";
+
+    std::cout << "Sum: " << 3 + 4 << "\n";
+    std::cout << "Minus: " << 3 - 4 << "\n";
+    std::cout << "Mult: " << 3 * 4 << "\n";
+    std::cout << "Div: " << 3 / 4 << "\n";
+    std::cout << "Div: " << 3.0 / 4.0 << "\n";
+
+    std::cout << "Float: " << 3.4 << "\n";
+
+    //std::cout << "Float: " << 3/0 << "\n";
+    std::cout << "Wtf: " << "12345" + 3 << "\n";
+
+    auto x = "Hello: ";
+    auto y = 7;
+    auto z = 'a';
+    std::cout << x << y << z << "\n";
+
+    int i = 2 + 2;
+    cout << i << "\n";
+    i = i * 3;
+    cout << i << "\n";
+
+    int j = 2;
+    cout << j << "\n";
+    i = 4.3;
+    j = 9 / 5;
+    cout << i << " " << j << "\n";
+
+    float k = 4.9;
+    cout << k << "\n";
+    k = 9.0 / 5;
+    cout << k << "\n";
+
+    cout << "\n Enter a number: \n";
+    cin >> i;
+    cout << i * 2 << "\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
